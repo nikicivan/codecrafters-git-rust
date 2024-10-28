982cf7b1612e8e58f72736651238d98babaaa429
nikicivan <ivan.nikic@gmail.com>
2024-10-28 10:18:13 +0100

    Read a blob object

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,75 @@
+use anyhow::{anyhow, Context, Result};
+use flate2::write::ZlibDecoder;
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
-use std::fs;
+use std::{
+    fs,
+    io::{stdout, Write},
+};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+static OBJECT_HEADER_PREFIX: &str = "blob ";
 
-    // Uncomment this block to pass the first stage
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+
+    let mut stdout = stdout();
+
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+        "cat-file" => {
+            assert_eq!(args[2], "-p");
+            let blog_sha = &args[3];
+            let blob_path = format!(".git/objects/{}/{}", &blog_sha[..2], &blog_sha[2..]);
+
+            let raw_content =
+                fs::read(&blob_path).with_context(|| format!("failed to read file {blob_path}"))?;
+
+            let mut decoder = ZlibDecoder::new(vec![]);
+            decoder.write_all(&raw_content).with_context(|| {
+                format!("failed to finish zlib decoder for object file at {blob_path}")
+            })?;
+
+            let decompressed_content = decoder.finish().with_context(|| {
+                format!("failed to finish zlib decoder for object file at {blob_path}")
+            })?;
+
+            let [header, content]: [&[_]; 2] = decompressed_content
+                .splitn(2, |b| b == &b'\0')
+                .collect::<Vec<_>>()
+                .try_into()
+                .map_err(|_| {
+                    anyhow!(
+                        "invalid object file at {blob_path}: expected it to contain {:?}",
+                        "\0"
+                    )
+                })?;
+
+            assert_eq!(
+                &header[..OBJECT_HEADER_PREFIX.len()],
+                OBJECT_HEADER_PREFIX.as_bytes()
+            );
+
+            let content_size: usize =
+                String::from_utf8(header[OBJECT_HEADER_PREFIX.len()..].to_vec())
+                    .with_context(|| format!("failed to parse object file header as utf8"))?
+                    .parse()
+                    .with_context(|| format!("failed to parse object file header as integer"))?;
+
+            assert_eq!(content.len(), content_size);
+
+            stdout.write_all(&content).with_context(|| {
+                format!("failed to write object file content to stdout for {blob_path}")
+            })?;
+        }
+        command => println!("unknown command: {}", command),
     }
+
+    Ok(())
 }
